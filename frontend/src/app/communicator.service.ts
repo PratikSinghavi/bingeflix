51db03259284131d8508cb75130004d4b61840ac
PratikSinghavi <ptksinghavi@gmail.com>
2022-04-07 20:43:12 -0700

    moved from GCP to new backend on z500

--- frontend/src/app/communicator.service.ts
+++ frontend/src/app/communicator.service.ts
@@ -17,9 +17,9 @@ export class CommunicatorService {
 
 
   // return this.http.get(`http://localhost:8080/${what}`);
-  // return this.http.get(`http://103.121.74.162:9095/${what}`);
+  return this.http.get(`https://backendz500.duckdns.org/${what}`);
   
-  return this.http.get(`https://angnodehw-309323.wl.r.appspot.com/${what}`);
+  // return this.http.get(`https://angnodehw-309323.wl.r.appspot.com/${what}`);
 
 
   }
